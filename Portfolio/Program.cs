659262b0050552e199fb988b43bc4c9952c08e6f
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-05-08 23:35:26 +0300

    Controllers added.

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
