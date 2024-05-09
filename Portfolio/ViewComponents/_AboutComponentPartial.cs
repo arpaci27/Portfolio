006661b2a6a48568ea0922002dcbd1c4d58f6a24
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-05-10 00:13:26 +0300

    Gathering info from database

--- Portfolio/ViewComponents/_AboutComponentPartial.cs
+++ Portfolio/ViewComponents/_AboutComponentPartial.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DAL.Context;
 
 namespace Portfolio.ViewComponents
 {
     public class _AboutComponentPartial: ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
             return View();
         }
     }
--- Portfolio/ViewComponents/_FeatureComponentPartial.cs
+++ Portfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DAL.Context;
 
 namespace Portfolio.ViewComponents
 {
     public class _FeatureComponentPartial: ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext(); 
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioContext.Features.ToList();
+            return View(values);
         }
     }
 }
