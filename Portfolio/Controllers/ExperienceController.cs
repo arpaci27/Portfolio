8add1803dd555c3ec4e8afbfe47f873337d1d5a3
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-12 16:58:37 +0300

    Admin Panel layout added.

---
+++ Portfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
