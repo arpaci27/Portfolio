ac1fbfad7a991cbaa2f73e1eb0fe9e1c5fe78ac6
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-13 23:45:50 +0300

    Layout Index seperated into partials

---
+++ Portfolio/ViewComponents/LayoutViewComponents/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutHeadComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ Portfolio/ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutSidebarComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
